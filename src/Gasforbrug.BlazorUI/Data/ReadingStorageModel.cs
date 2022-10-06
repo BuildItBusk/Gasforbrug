438f29f7ca8362ce0b2e85c21545cc1b5386cc9f
Jakob Busk Sørensen <busk.soerensen@gmail.com>
2022-10-06 14:03:16 +0200

    The reading storage model class has been moved to it's own file and namespace, so that it can be shared between components.

---
+++ src/Gasforbrug.BlazorUI/Data/ReadingStorageModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gasforbrug.BlazorUI.Data
+{
+    public sealed class ReadingStorageModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Skal være et positivt tal.")]
+        public decimal Value { get; set; }
+    }
+}
